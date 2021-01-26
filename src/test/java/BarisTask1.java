f6de092bf1c04c6a84e4fc44e1e7544b996b1bd6
Baris312 <barmagan81@gmail.com>
2021-01-26 15:27:03 -0600

    1

--- src/test/java/BarisTask1.java
+++ src/test/java/BarisTask1.java
@@ -2,7 +2,7 @@ public class BarisTask1
     {
         public static void main(String[] args)
             {
-                System.out.println("Baris Test Case");
+                System.out.println("CRM24 Task");
             }
 
 
